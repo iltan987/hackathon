d521b008c92a32483b4c818b300721e6440d89e2
nazhin1382 <nazhin.abdolbaghi@gmail.com>
2024-12-11 05:52:10 -0800

    current location added

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,25 +1,27 @@
 import HomeIcon from '@mui/icons-material/Home';
 import MapIcon from '@mui/icons-material/Map';
-import SearchIcon from '@mui/icons-material/Search';
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 export default function BottomNavigation() {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white p-3 shadow-lg flex justify-around items-center">
       {/* Home Icon with Label */}
       <div className="flex-1 flex flex-col justify-center items-center">
-        <HomeIcon />
+        <div className="p-2 rounded-full hover:bg-purple-100 transition duration-200">
+          <HomeIcon className="text-gray-600 hover:text-purple-500" />
+        </div>
         <span className="text-sm text-gray-600 mt-1">Home</span>
       </div>
-      {/* Search Icon with Highlight */}
       <div className="flex-1 flex flex-col justify-center items-center">
-        <div className="w-12 h-12 bg-purple-500 text-white rounded-full flex justify-center items-center shadow-lg">
-          <SearchIcon />
+        <div className="w-12 h-12 bg-purple-500 text-white rounded-full flex justify-center items-center shadow-lg hover:bg-purple-600 transition duration-200">
+          <LocationOnIcon />
         </div>
-        <span className="text-sm text-gray-600 mt-1">Search</span>
+        <span className="text-sm text-gray-600 mt-1">Current Location</span>
       </div>
       {/* Map Icon with Label */}
       <div className="flex-1 flex flex-col justify-center items-center">
-        <MapIcon />
+        <div className="p-2 rounded-full hover:bg-purple-100 transition duration-200">
+          <MapIcon className="text-gray-600 hover:text-purple-500" />
+        </div>
         <span className="text-sm text-gray-600 mt-1">Map</span>
       </div>
     </div>
